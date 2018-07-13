f9ab044e49e3d67b5beef0c74ab29329896f7c9c
GuilhermeARS <guiars@hotmail.com>
2018-07-13 19:35:51 -0300

    R

--- app/src/main/java/com/es/david/vacinas/activity/LoginActivity.java
+++ app/src/main/java/com/es/david/vacinas/activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.es.david.vacinas.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.es.david.vacinas.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
