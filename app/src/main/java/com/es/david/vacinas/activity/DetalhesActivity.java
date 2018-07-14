3249609bc2390c4306260e1f74056e61c75e24da
GuilhermeAlves <guiars@hotmail.com>
2018-07-13 22:31:02 -0300

    fix R

--- app/src/main/java/com/es/david/vacinas/activity/DetalhesActivity.java
+++ app/src/main/java/com/es/david/vacinas/activity/DetalhesActivity.java
@@ -2,27 +2,12 @@ package com.es.david.vacinas.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.es.david.vacinas.R;
-import com.es.david.vacinas.adapter.Adapter;
 import com.es.david.vacinas.modelo.Vacina;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DetalhesActivity extends AppCompatActivity {
     Vacina vacina;
